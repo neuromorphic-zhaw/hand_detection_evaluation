0bc11f3d0371ac0a55eec0521c28a137dd172dcc
Stefan Glüge <glue@zhaw.ch>
2024-02-09 16:53:38 +0100

    added code to run model on lager dataset

---
+++ create_iterable_dataset.py
@@ -0,0 +1,121 @@
+import os
+import numpy as np
+import torch
+from scipy.sparse import csr_matrix
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter1d
+import pickle
+
+def target_coords_to_onehot_smoothed_lowres(target_coords_by_cam, img_height=260, img_width=344, sigma=5, scale=True, downsample_factor=2):
+    # target_coords_by_cam.shape
+    num_joints, num_cords = target_coords_by_cam.shape
+    
+    if downsample_factor is not None:
+        target_coords_by_cam_onehot = torch.zeros(num_joints, int((img_height + img_width)/downsample_factor))
+    else:
+        target_coords_by_cam_onehot = torch.zeros(num_joints, img_height + img_width)
+    # target_coords_by_cam_onehot.shape # joints x vec_length
+    
+    # joint_idx = 0
+    # set the target coordinates to 1 from the target coordinates
+    for joint_idx in range(num_joints):
+        y_coord = int(target_coords_by_cam[joint_idx, 0])
+        x_coord = int(target_coords_by_cam[joint_idx, 1])
+    
+        y_coord_one_hot = torch.zeros(img_height)
+        y_coord_one_hot[y_coord] = 1
+
+        x_coord_one_hot = torch.zeros(img_width)
+        x_coord_one_hot[x_coord] = 1
+
+        # smooth the one hot encoding
+        y_coord_one_hot_smoothed = gaussian_filter1d(y_coord_one_hot, sigma=sigma)
+        x_coord_one_hot_smoothed = gaussian_filter1d(x_coord_one_hot, sigma=sigma)
+        if scale:
+            y_coord_one_hot_smoothed = y_coord_one_hot_smoothed / np.max(y_coord_one_hot_smoothed)
+            x_coord_one_hot_smoothed = x_coord_one_hot_smoothed / np.max(x_coord_one_hot_smoothed)
+
+        # plt.plot(y_coord_one_hot, label='original', color='b', linestyle='', marker='.')
+        # plt.plot(y_coord_one_hot_smoothed, label='smoothed', color='r')
+        # plt.show()
+        if downsample_factor is not None:
+            y_coord_one_hot_smoothed = y_coord_one_hot_smoothed[::downsample_factor]
+            x_coord_one_hot_smoothed = x_coord_one_hot_smoothed[::downsample_factor]
+                
+            # plt.plot(y_coord_one_hot_smoothed, label='downsampled', color='g')
+            # plt.show()
+            # y_coord_one_hot_smoothed.shape
+            # plt.plot(x_coord_one_hot_smoothed, label='downsampled', color='g')
+            # plt.show()
+            # x_coord_one_hot_smoothed.shape
+            xy_coords_one_hot_smoothed = torch.concat([torch.tensor(y_coord_one_hot_smoothed), torch.tensor(x_coord_one_hot_smoothed)])
+            # xy_coords_one_hot_smoothed.shape
+
+            target_coords_by_cam_onehot[joint_idx, :] = xy_coords_one_hot_smoothed
+
+    # make flatt layout
+    target_coords_by_cam_onehot_flatt = target_coords_by_cam_onehot.view(-1)
+    
+    return target_coords_by_cam_onehot_flatt
+
+
+project_path = './'
+model_path = project_path + './model/train/'
+event_data_path = project_path + './data/dhp19_samples/'
+# paramters of the traininf data
+img_width = 344
+img_height = 260
+downsample_factor = 2
+xy_coord_vec_length = int((img_width + img_height)/downsample_factor)
+
+# Create Dataset instance
+cam_idxs = [1,2] # camera index to train on 1,2 are frontal views
+seq_length = 8 # number of event frames per sequence to be shown to the SDNN
+joint_idxs = [7, 8] # joint indices to train on
+num_joints = len(joint_idxs)
+
+files = os.listdir(event_data_path)
+files = [f for f in files if f.endswith('pt')]
+
+cam_id = cam_idxs[1]
+# for cam_id in cam_idxs:
+
+input_frames = []
+target_vectors = []
+sessions = []
+subjects = []
+movs = []
+
+# file = files[0]
+# loop throug files
+for i, file in enumerate(files):
+    session = int(file.split('_')[1][7:])
+    subject = int(file.split('_')[0][1:])
+    mov = int(file.split('_')[2][3:])    
+    data_dict = torch.load(event_data_path + file) # load file
+    # loop over all time steps in the file and select input frame and target joints for cam N
+    # t=0
+    for t in range(seq_length):
+        # select and tranform input frame
+        act_input_tensor = data_dict['input_tensor'].to_dense()[0,:,:,cam_id,t].numpy().astype(np.float32) # take frame t of cam N, reshape to [260, 340]
+        # act_input_tensor.shape
+        # plt.imshow(act_input_tensor[:,:])
+        # plt.colorbar()
+        # select and tranform target coords
+        target_coords_abs_by_cam = data_dict['target_coords_abs'][joint_idxs,:,cam_id,t] # take target joints for time t of cam N
+        # target_coords_abs_by_cam.shape # joints, cam
+        act_targets_1hot = target_coords_to_onehot_smoothed_lowres(target_coords_abs_by_cam)
+        
+        # save input frame, target_1hot, session, subject and mov
+        input_frames.append(csr_matrix(act_input_tensor))  # save input frame as sparse matrix
+        target_vectors.append(act_targets_1hot.numpy().astype(np.float32)) # [0] 
+        sessions.append(session)
+        subjects.append(subject)
+        movs.append(mov)
+
+# store the data in a dictionary
+data_dict = {'input_frames': input_frames, 'target_vectors': target_vectors, 'sessions': sessions, 'subjects': subjects, 'movs': movs}
+# save the dictionary as a .pkl file
+with open(event_data_path + 'dhp19_data_subject1_cam' + str(cam_id) + '.pkl', 'wb') as f:
+    pickle.dump(data_dict, f)
+
--- eval_cpu.py
+++ eval_cpu.py
@@ -1,63 +1,68 @@
-# run infercence our traine DHP19 model on Loihi?
+# run infercence our trained DHP19 model on Loihi?
 from lava.lib.dl import netx
 import logging
-import torch
 import numpy as np
-from dataset import DHP19NetDataset
+
 from lava.proc import io
-# from plot import plot_input_sample, plot_output_vs_target, plot_input_vs_prediction_vs_target, show_model_output
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from lava.magma.core.run_conditions import RunSteps
 from lava.magma.core.run_configs import Loihi2HwCfg, Loihi2SimCfg
 from lava.utils.system import Loihi2
-from dataset import DHP19NetDataset
 from utils import generate_movement_name_df, comp_dist_for_sample
+import pickle
 
 
-
-# def get_xy_coords_for_prediction_and_target(prediction, target, joint, downsample_factor=2, img_height=260, img_width=344):
-#     xy_coord_vec_length = int((img_width + img_height)/downsample_factor) # 302
-
-#     coords_1hot = prediction[joint*xy_coord_vec_length:(joint+1)*xy_coord_vec_length]
-#     coords_one_hot_y1 = coords_1hot[0:int(img_height / downsample_factor)]
-#     coords_one_hot_x1 = coords_1hot[int(img_height / downsample_factor):]
+class DHP19pklDataset():
+    def __init__(self, path, cam_id=1):
+        self.path = path
+        self.cam_id = cam_id
+        # load data from pickle file
+        with open(path, 'rb') as f:
+            data_dict = pickle.load(f)
+            
+            self.input_frames = data_dict['input_frames']
+            self.target_vectors = data_dict['target_vectors']
+            self.sessions = data_dict['sessions']
+            self.subjects = data_dict['subjects']
+            self.movs = data_dict['movs']
     
-#     # get prediction from model output
-#     predicted_y1 = np.where(coords_one_hot_y1 == coords_one_hot_y1.max())[0][0]
-#     predicted_x1 = np.where(coords_one_hot_x1 == coords_one_hot_x1.max())[0][0]
-#     predicted_y1 = predicted_y1 * downsample_factor
-#     predicted_x1 = predicted_x1 * downsample_factor
-
-#     # get parts of the target data by coordinate and joint
-#     target_1hot = target[joint*xy_coord_vec_length:(joint+1)*xy_coord_vec_length]
-#     target_one_hot_y1 = target_1hot[0:int(img_height / downsample_factor)]
-#     target_one_hot_x1 = target_1hot[int(img_height / downsample_factor):]
+    def __len__(self):
+        return len(self.input_frames)
     
-#     # get prediction from target
-#     target_y1 = np.where(target_one_hot_y1 == target_one_hot_y1.max())[0][0]
-#     target_x1 = np.where(target_one_hot_x1 == target_one_hot_x1.max())[0][0]    
-#     target_y1 = target_y1 * downsample_factor
-#     target_x1 = target_x1 * downsample_factor
+    def __getitem__(self, idx):
+        input_frame = self.input_frames[idx].todense() # 260 x 344
+        input_frame = np.expand_dims(input_frame, axis=-1).transpose(1,0,2) # 344 x 260 x 1
 
-#     return predicted_y1, predicted_x1, target_y1, target_x1
+        return input_frame, self.target_vectors[idx], self.sessions[idx], self.subjects[idx], self.movs[idx]
 
 
-# def comp_dist_for_sample(prediction, target, downsample_factor=2, img_height=260, img_width=344):
-#     prediction.shape # (604,0)
-#     target.shape # (604,0)
-#     xy_coord_vec_length = int((img_width + img_height)/downsample_factor) # 302
+def get_prediction_from_1hot_vector(vector_1hot, downsample_factor=2, img_height=260, img_width=344):
 
-#     # get xy coords of prediction and target per joint
-#     predicted_y1, predicted_x1, target_y1, target_x1 = get_xy_coords_for_prediction_and_target(prediction, target, joint=0, downsample_factor=2, img_height=260, img_width=344)
-#     predicted_y2, predicted_x2, target_y2, target_x2 = get_xy_coords_for_prediction_and_target(prediction, target, joint=1, downsample_factor=2, img_height=260, img_width=344)
-
-#     # compute distance between prediction and target
-#     dist1 = np.sqrt((predicted_y1 - target_y1)**2 + (predicted_x1 - target_x1)**2)
-#     dist2 = np.sqrt((predicted_y2 - target_y2)**2 + (predicted_x2 - target_x2)**2)
-
-#     return dist1, dist2
-
+    # plot model output  as annimation
+    xy_coord_vec_length = int((img_width + img_height)/downsample_factor)
+    
+    # get parts of the output data by coordinate and joint
+    joint = 0    
+    coords_1hot = vector_1hot[joint*xy_coord_vec_length:(joint+1)*xy_coord_vec_length]
+    coords_one_hot_y1 = coords_1hot[0:int(img_height / downsample_factor)]
+    coords_one_hot_x1 = coords_1hot[int(img_height / downsample_factor):]
+    
+    coords_one_hot_y1.shape
+    # get max 
+    max_y1 = np.where(coords_one_hot_y1 == coords_one_hot_y1.max())[0][0]
+    max_x1 = np.where(coords_one_hot_x1 == coords_one_hot_x1.max())[0][0]
+    
+    joint = 1    
+    coords_1hot = vector_1hot[joint*xy_coord_vec_length:(joint+1)*xy_coord_vec_length]
+    coords_one_hot_y2 = coords_1hot[0:int(img_height / downsample_factor)]
+    coords_one_hot_x2 = coords_1hot[int(img_height / downsample_factor):]
+    # get max
+    max_y2 = np.where(coords_one_hot_y2 == coords_one_hot_y2.max())[0][0]
+    max_x2 = np.where(coords_one_hot_x2 == coords_one_hot_x2.max())[0][0]
+    
+    return max_y1, max_x1, max_y2, max_x2, coords_one_hot_y1, coords_one_hot_x1, coords_one_hot_y2, coords_one_hot_x2
 
 
 if __name__ == '__main__':      
@@ -69,12 +74,13 @@ if __name__ == '__main__':
     print("Loihi2 compiler is not available in this system. "
         "This tutorial will execute on CPU backend.")
     compression = io.encoder.Compression.DENSE
-    system = 'cpu_quand_input'
+    system = 'cpu_last100'
     
     # Set paths to model and data
     project_path = './'
     model_path = project_path + './model/train/'
-    event_data_path = project_path + './data/dhp19_samples/'
+    cam_id = 1
+    event_data_path = project_path + 'data/dhp19_samples/' + 'dhp19_data_subject1_cam' + str(cam_id) + '.pkl'
     # paramters of the traininf data
     img_width = 344
     img_height = 260
@@ -104,34 +110,32 @@ if __name__ == '__main__':
                     '_lam' + str(lam)
 
     act_model_path = model_path + experiment_name + '/'
-    net = netx.hdf5.Network(net_config=act_model_path + 'model.net', skip_layers=1)
     
-    # print(net)
-    len(net)
-    # net.inp.shape
-    # net.out.shape
-    # net.input_message_bits
-    # net.output_message_bits
-    # net.spike_exp
-    # net.in_layer.output_message_bits
-    # net.out_layer.neuron
+    print('Loading net ' + experiment_name    )
+    net = netx.hdf5.Network(net_config=act_model_path + 'model.net', skip_layers=1)
 
-    # # print('Loading net ' + experiment_name    )
-    complete_dataset = DHP19NetDataset(path=event_data_path, joint_idxs=joint_idxs, cam_id=cam_idxs[0], num_time_steps=seq_length)
+    # Load dataset    
+    complete_dataset = DHP19pklDataset(path=event_data_path)
     print('Dataset loaded: ' + str(len(complete_dataset)) + ' samples found')
     
+
     # get names of movements based on session and movement number
     movement_names_df = generate_movement_name_df()
 
-    # # show sample from the dataset
-    # for i in range(0, 100):
-    #     input, target, session, subject, mov = complete_dataset[i]
-    #     print('Session: ' + str(session) + ', Subject: ' + str(subject) + ', Movement: ' + str(mov) + ', ' + movement_names_df.loc[(movement_names_df['session'] == session) & (movement_names_df['mov'] == mov), 'mov_string'].iloc[0])   
-    # # plot_input_sample(np.swapaxes(input[:,:,0],0,1), target_coords=None, title='Input frame', path=None)
-    
+    # show sample from the dataset
+    # input, target, session, subject, mov = complete_dataset[4000]   
+    # input.shape
+    # plt.imshow(input, cmap='gray')
+    # plt.colorbar()
+    # # # print('Session: ' + str(session) + ', Subject: ' + str(subject) + ', Movement: ' + str(mov) + ', ' + movement_names_df.loc[(movement_names_df['session'] == session) & (movement_names_df['mov'] == mov), 'mov_string'].iloc[0])   
+
+
+    # setup run conditions
+    num_steps = 100 #len(complete_dataset)
+
     # setup lava process modules
     quantize = netx.modules.Quantize(exp=6)  # convert to fixed point representation with 6 bit of fraction
-    sender = io.injector.Injector(shape=net.inp.shape, buffer_size=128)
+    sender = io.injector.Injector(shape=net.inp.shape, buffer_size=num_steps-8)
     encoder = io.encoder.DeltaEncoder(shape=net.inp.shape,
                                   vth=net.net_config['layer'][0]['neuron']['vThMant'],
                                   spike_exp=6,
@@ -139,7 +143,7 @@ if __name__ == '__main__':
                                   compression=compression)
     
     sender.out_port.shape
-    receiver = io.extractor.Extractor(shape=net.out.shape, buffer_size=128)
+    receiver = io.extractor.Extractor(shape=net.out.shape, buffer_size=num_steps-8)
     dequantize = netx.modules.Dequantize(exp=net.spike_exp + 12, num_raw_bits=24)
 
     # connect modules
@@ -147,11 +151,13 @@ if __name__ == '__main__':
     encoder.s_out.connect(net.inp)
     net.out.connect(receiver.in_port)
 
-    # setup run conditions
-    num_steps = len(complete_dataset)
+    # setup display of the encoded input
+    encoder_output_extractor = io.extractor.Extractor(shape=net.inp.shape, buffer_size=num_steps-8)
+    encoder.s_out.connect(encoder_output_extractor.in_port)
+
     # num_steps = 40
     run_condition = RunSteps(num_steps=num_steps, blocking=False)
-    
+        
     exception_proc_model_map = {io.encoder.DeltaEncoder: io.encoder.PyDeltaEncoderModelDense}
     run_config = Loihi2SimCfg(select_tag='fixed_pt',
                             exception_proc_model_map=exception_proc_model_map)
@@ -166,14 +172,20 @@ if __name__ == '__main__':
     eval_df['sum dist joint 1'] = 0
     eval_df['sum dist joint 2'] = 0
 
-    for t in range(num_steps):
+    input_list = []
+    output_dequand_list = []
+    output_raw_list = []
+    # encoder_output_list = []
+    target_list = []   
+
+    len(complete_dataset)-num_steps
+    for t in range(len(complete_dataset)-num_steps, len(complete_dataset)):
+    # for t in range(num_steps):
         print('t = ' + str(t))
 
         input, target, session, subject, mov  = complete_dataset[t]
-        # print('Session: ' + str(session) + ', Subject: ' + str(subject) + ', Movement: ' + str(mov) + ', ' + movement_names_df.loc[(movement_names_df['session'] == session) & (movement_names_df['mov'] == mov), 'mov_string'].iloc[0])
-        # input_quantized = quantize(input)
         sender.send(quantize(input))        # This sends the input frame to the Lava network
-        # sender.send(input)        # This sends the input frame to the Lava network
+        # encoder_output = encoder_output_extractor.receive()           
         model_out = receiver.receive()  # This receives the output from the Lava network
         out_dequantized = dequantize(model_out)
         
@@ -186,12 +198,11 @@ if __name__ == '__main__':
         eval_df.loc[(movement_names_df['session'] == session) & (movement_names_df['mov'] == mov), 'sum dist joint 1'] += dist1
         eval_df.loc[(movement_names_df['session'] == session) & (movement_names_df['mov'] == mov), 'sum dist joint 2'] += dist2
         
-        # show_model_output(out_dequantized, downsample_factor=2, img_height=260, img_width=344, time_step=t)   
-        # plot_output_vs_target(out_dequantized, target, downsample_factor=2, img_height=260, img_width=344, time_step=t, filename='plots/output_vs_target' + str(t) + '_' + system + '.png')
-        # plot_output_vs_target(out_dequantized, target, downsample_factor=2, img_height=260, img_width=344, time_step=t, filename=None)
-        # plot_input_vs_prediction_vs_target(input, out_dequantized, target, downsample_factor=2, img_height=260, img_width=344, time_step=t, filename='plots/input_vs_prediction_vs_target_' + str(t) + '_' + system + '.png')
-        
-        
+        input_list.append(input)
+        # encoder_output_list.append(encoder_output)
+        output_dequand_list.append(out_dequantized)
+        output_raw_list.append(model_out)
+        target_list.append(target)
 
     sender.wait()
     sender.stop()
@@ -202,3 +213,77 @@ if __name__ == '__main__':
     eval_df['mean dist joint 2'] = eval_df['sum dist joint 2'] / eval_df['fame count']
     print(eval_df)
     eval_df.to_csv('eval_df_' + system + '.csv')
+
+     # Create animation of model outputs
+    target_y1, target_x1, target_y2, target_x2, target_one_hot_y1, target_one_hot_x1, target_one_hot_y2, target_one_hot_x2 = get_prediction_from_1hot_vector(target_list[78], downsample_factor=2, img_height=260, img_width=344)
+    prediction_y1, prediction_x1, prediction_y2, prediction_x2, output_one_hot_y1, output_one_hot_x1, output_one_hot_y2, output_one_hot_x2 = get_prediction_from_1hot_vector(output_dequand_list[78], downsample_factor=2, img_height=260, img_width=344)
+        
+    # np.where(output_one_hot_x1 == output_one_hot_x1.max())[0][0]
+    # prediction_x1
+    # plt.plot(output_one_hot_x1)
+
+    ylim = (-1, 1)
+
+    fig, axs = plt.subplots(2, 2)
+    y1_plot = axs[0, 0].plot(output_one_hot_y1)
+    y1_target = axs[0,0].axvline(target_y1, color='g', ls='--')
+    y1_predict = axs[0,0].axvline(prediction_y1, color='r', ls='--')
+    # y1.vlines(predicted_y1, ymin=0, ymax=1, colors='r', linestyles='dashed', label='predicted')
+    axs[0, 0].set_ylabel('y1')
+    axs[0, 0].set_title('Joint 1')
+    axs[0, 0].set_ylim(ylim)
+
+    x1_plot = axs[0, 1].plot(output_one_hot_x1)
+    x1_target = axs[0,1].axvline(target_x1, color='g', ls='--')
+    x1_predict = axs[0,1].axvline(prediction_x1, color='r', ls='--')
+    axs[0, 1].set_ylabel('x1')
+    axs[0, 1].set_ylim(ylim)
+
+    y2_plot = axs[1, 0].plot(output_one_hot_y2)
+    y2_target = axs[1,0].axvline(target_y2, color='g', ls='--')
+    y2_predict = axs[1,0].axvline(prediction_y2, color='r', ls='--')
+    axs[1, 0].set_ylabel('y2')
+    axs[1, 0].set_title('Joint 2')
+    axs[1, 0].set_ylim(ylim)
+
+    x2_plot = axs[1, 1].plot(output_one_hot_x2)
+    x2_target = axs[1,1].axvline(target_x2, color='g', ls='--')
+    x2_predict = axs[1,1].axvline(prediction_x2, color='r', ls='--')
+    axs[1, 1].set_ylabel('x2')
+    axs[1, 1].set_ylim(ylim)
+
+    title = axs[0, 0].text(0.5,0.85, "frame 0", bbox={'facecolor':'w', 'alpha':0.5, 'pad':5}, ha="center")
+    fig.tight_layout()
+
+    def update(frame):
+            # for each frame, update the data stored on each artist.
+        target_y1, target_x1, target_y2, target_x2, target_one_hot_y1, target_one_hot_x1, target_one_hot_y2, target_one_hot_x2 = \
+            get_prediction_from_1hot_vector(target_list[frame], downsample_factor=2, img_height=260, img_width=344)
+        prediction_y1, prediction_x1, prediction_y2, prediction_x2, output_one_hot_y1, output_one_hot_x1, output_one_hot_y2, output_one_hot_x2 = \
+            get_prediction_from_1hot_vector(output_dequand_list[frame], downsample_factor=2, img_height=260, img_width=344)
+
+        title.set_text('frame ' + str (frame))
+        y1_plot[0].set_ydata(output_one_hot_y1)
+        y1_target.set_xdata(target_y1)
+        y1_predict.set_xdata(prediction_y1)
+        
+        x1_plot[0].set_ydata(output_one_hot_x1)
+        x1_target.set_xdata(target_x1)
+        x1_predict.set_xdata(prediction_x1)
+        
+        y2_plot[0].set_ydata(output_one_hot_y2)
+        y2_target.set_xdata(target_y2)
+        y2_predict.set_xdata(prediction_y2)
+
+        x2_plot[0].set_ydata(output_one_hot_x2)
+        x2_target.set_xdata(target_x2)
+        x2_predict.set_xdata(prediction_x2)
+        
+        return y1_plot, x1_plot, y2_plot, x2_plot, y1_target, y1_predict
+
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=len(output_dequand_list), interval=200)
+    ani.save(filename='model_outputs_' + system + '.mp4', writer='ffmpeg')
+        
+    # encoder_output = encoder_output_list[45]
+    # plt.imshow(encoder_output, cmap='gray')
+    # plt.colorbar()
